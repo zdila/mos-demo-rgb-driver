629f0521c57cfaf1c3b29ba5dd5ad72f6df5445e
Martin Ždila <m.zdila@gmail.com>
2020-02-29 15:49:56 +0100

    cleanup

--- fs/init.js
+++ fs/init.js
@@ -8,31 +8,32 @@ let pins = {
   b: Cfg.get('rgb.pin.b')
 };
 
-RPC.addHandler('setRGB', function (args) {
-  print("RGB", JSON.stringify(args));
+RPC.addHandler('setRGB', function(args) {
+  print('RGB', JSON.stringify(args));
+
   if (
-    typeof args === 'object'
-    && isValidBand(args.r)
-    && isValidBand(args.g)
-    && isValidBand(args.b)
+    typeof args === 'object' &&
+    isBandValid(args.r) &&
+    isBandValid(args.g) &&
+    isBandValid(args.b)
   ) {
-    PWM.set(pins.r, freq(args.r), duty(args.r));
-    PWM.set(pins.g, freq(args.g), duty(args.g));
-    PWM.set(pins.b, freq(args.b), duty(args.b));
+    PWM.set(pins.r, 1000, duty(args.r));
+    PWM.set(pins.g, 1000, duty(args.g));
+    PWM.set(pins.b, 1000, duty(args.b));
 
     return null;
   } else {
-    print("Invalid parameters.");
-    return { error: -32602, message: 'Bad request. Expected: {"r":<R>,"g":<G>,"b":<B>}' };
+    print('Invalid parameters.');
+
+    return {
+      error: -32602,
+      message: 'Bad request. Expected: {"r":<R>,"g":<G>,"b":<B>}'
+    };
   }
 });
 
-function isValidBand(v) {
+function isBandValid(v) {
   return typeof v === 'number' && v >= 0 && v <= 1;
-}
-
-function freq(v) {
-  return duty(v) < 0.005 ? 0 : 400;
 }
 
 function duty(v) {
